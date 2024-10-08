1ac9079d9cbd5ad31314f25adf7161f6fc6f9238
Mr.Black01 <purus5718@gmail.com>
2024-10-08 14:47:05 +0530

    2nd commit

--- database/migrations/2024_10_08_082257_add_created_by_to_businesses_table.php
+++ database/migrations/2024_10_08_082257_add_created_by_to_businesses_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('businesses', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->integer('user_id');
-            $table->timestamps();
-
-
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -26,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('businesses');
+        Schema::table('businesses', function (Blueprint $table) {
+            //
+        });
     }
 };
